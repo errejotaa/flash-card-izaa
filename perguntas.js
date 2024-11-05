dea9beae2fd2893b8683bdc1597239465cbed8c9
errejotaa <00001120666156sp@al.educacao.sp.gov.br>
2024-11-05 15:12:30 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'O que é lixo?',
+    'todo e qualquer material considerado inútil, e/ou sem valor, gerado pela atividade humana, o qual precisa ser eliminado'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é cromossomo?',
+    'estruturas que abrigam o material genético dentro da célula.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como dizer oi em ingles?',
+    'Oi em ingles é HI (RAI)'
+)
